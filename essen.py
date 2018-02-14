a395c65c19bd71c3f6813cc2e1636bb3d7c8384f
Solotalent <36490644+Solotalent84@users.noreply.github.com>
2018-02-14 23:41:09 +0100

    Update essen.py

--- essen.py
+++ essen.py
@@ -1,5 +1,12 @@
 
-essen = ['einen Doener', 'einen Wirsingeintopf', 'einen Lahmacun', 'eine Portion Kartoffelbrei',
+essen = ['einen Wirsingeintopf', 'einen Lahmacun', 'eine Portion Kartoffelbrei',
                      'Spinat', 'Astronautenfutter', 'einen Snickers', 'einen Mars', 'eine Brezel',
                      'eine Portion Gagh', 'Filet Mignon', 'ein Döner', 'ein Teller Sphagetti Blogonaise',
-                     'ein Teller Sphagetti Käsesahnesoße']
+                     'ein Teller Sphagetti Käsesahnesoße', 'einen Obstteller', 'eine Hand voll Gummibärchen',
+                     'eine gummiartiges Hühnchen', 'eine Schale mit Kartoffelchips', 'zwei Käsebrote',
+                     'Okonomiyaki', 'zwei Stück Erdbeertorte', 'feuriges Chili con Carne', 'ein fluffiges Rührei',
+                     'eine leckere Pizza', 'einen frischen Gurkensalat', 'einen großen Chefsalat',
+                     'eine übelriechende Käsesuppe', 'Fischstäbchen und Kartoffelbrei', 'ungarisches Gulasch',
+                     'Nudeln mit Soja-Bolognese', 'ein original französisches Ratatouille', 'einen sahnigen Schokopudding',
+                     'arme Ritter', 'einen Festtagsbraten mit Klößen', 'Pommes mit Majo', 'corvidaes leckeren Kartoffelauflauf'
+                      ]
